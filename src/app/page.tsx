dabc1a5006029bea1b504b5f99a5fbd3328ce196
kinako1415 <Owata09@outlook.jp>
2025-03-05 15:28:31 +0900

    背景動画の作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import styles from "./page.module.scss";
+
 export default function Home() {
   return (
     <div>
-      <div></div>
+      <video className={styles.video} autoPlay loop muted playsInline>
+        <source src="/cafe.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       <div></div>
     </div>
   );
