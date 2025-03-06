bb6199731d8fe3b1947672a082d32e0ca71a6b7b
kinako1415 <Owata09@outlook.jp>
2025-03-06 13:02:47 +0900

    スタイルの調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,37 @@
 "use client";
-import { motion } from "framer-motion";
+import { motion, useScroll } from "framer-motion";
 import About from "./../components/home/About";
 import styles from "./page.module.scss";
 import Scroll from "@/components/home/Scroll";
 export default function Home() {
+  const { scrollYProgress } = useScroll();
+
   const InView = {
     hidden: { y: "40px", opacity: 0 },
     visible: { y: "0px", opacity: 1 },
   };
 
+  console.log(scrollYProgress);
+
   return (
-    <div>
-      <video className={styles.video} autoPlay loop muted playsInline>
-        <source src="/cafe.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <div className={styles.filter}></div>
+    <motion.div>
+      <motion.div
+      // initial={{ scale: 0.8, radius: "100px 100px 100px 100px" }}
+      // animate={{ scale: 1, radius: "0px 0px 100px 100px" }}
+      // transition={{ duration: 0.4 }}
+      >
+        <video className={styles.video} autoPlay loop muted playsInline>
+          <source src="/cafe.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className={styles.filter}></div>
+      </motion.div>
       <div className={styles.titleContainer}>
         <motion.div
           variants={InView}
           initial="hidden"
           whileInView="visible"
-          transition={{ delay: 0.1 }}
+          transition={{ delay: 0.1, duration: 0.5 }}
           className={styles.title}
         >
           スタイリッシュに、自由に。
@@ -32,7 +42,7 @@ export default function Home() {
           variants={InView}
           initial="hidden"
           whileInView="visible"
-          transition={{ delay: 0.2 }}
+          transition={{ delay: 0.3, duration: 0.5 }}
           className={styles.subTitle}
         >
           Stylish and free. A new way of living with containers.
@@ -40,6 +50,6 @@ export default function Home() {
         <Scroll />
       </div>
       <About />
-    </div>
+    </motion.div>
   );
 }
