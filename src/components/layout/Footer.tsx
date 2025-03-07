4388327dc48d23da8f197a63dcdbb49f07a1d452
kinako1415 <Owata09@outlook.jp>
2025-03-07 23:35:30 +0900

    footerのrightContainerの作成

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,6 +5,13 @@ const Footer = () => {
   return (
     <div className={styles.container}>
       <div className={styles.logo}>MODBOX</div>
+      <div className={styles.rightContainer}>
+        <div>ラインナップ</div>
+        <div>オプション</div>
+        <div>お問い合わせ・お見積り</div>
+        <div>運搬・設置について</div>
+        <div>ご購入の流れ</div>
+      </div>
     </div>
   );
 };
