c27bdd06835f0222e5c7e80e0db19a587e7debf6
kinako1415 <Owata09@outlook.jp>
2025-03-07 23:23:24 +0900

    フッターの作成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, BIZ_UDPGothic } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
 const bizUdpGothic = BIZ_UDPGothic({
   weight: ["400", "700"],
@@ -36,6 +37,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+"use client";
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>MODBOX</div>
+    </div>
+  );
+};
+
+export default Footer;
